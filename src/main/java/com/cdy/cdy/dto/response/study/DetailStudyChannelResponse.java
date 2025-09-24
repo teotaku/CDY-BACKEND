470a519f596aa7be2863b130de70dfdf8b067d1d
teotaku <dongik9467@naver.com>
2025-09-24 22:53:57 +0900

    detailstudychannelresponde 잠깐 보류

--- src/main/java/com/cdy/cdy/dto/response/study/DetailStudyChannelResponse.java
+++ src/main/java/com/cdy/cdy/dto/response/study/DetailStudyChannelResponse.java
@@ -1,25 +1,25 @@
-package com.cdy.cdy.dto.response.study;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-
-import java.util.List;
-
-@Builder
-@Getter
-@AllArgsConstructor
-public class DetailStudyChannelResponse {
-
-    private String category;
-    private String userImageUrl;
-    private Long studyCount;
-
-    private
-
-    //스터디목록들
-    private List<>
-
-    //완료된 프로젝트
-    private List<>
-}
+//package com.cdy.cdy.dto.response.study;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//import java.util.List;
+//
+//@Builder
+//@Getter
+//@AllArgsConstructor
+//public class DetailStudyChannelResponse {
+//
+//    private String category;
+//    private String userImageUrl;
+//    private Long studyCount;
+//
+//    private
+//
+//    //스터디목록들
+//    private List<>
+//
+//    //완료된 프로젝트
+//    private List<>
+//}
